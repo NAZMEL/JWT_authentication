b1b88cd7843f17112259dd85bcb9a895a0ce5b89
NAZMEL <melnyknasar@gmail.com>
2022-07-24 13:46:57 +0300

    Add validator for registration

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const { body } = require("express-validator");
 const userController = require("../controllers/user-controller");
 
 const router = new Router();
 
-router.post("/registration", userController.registration);
+router.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 4, max: 16 }),
+  userController.registration
+);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 
