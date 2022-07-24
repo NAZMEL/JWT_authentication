33c02747a431dc99d7c465422ac5500055da60dd
NAZMEL <melnyknasar@gmail.com>
2022-07-24 12:09:15 +0300

    Start the server and install tools

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+const app = express();
+
+const start = async() =>{
+    try{
+        app.listen(PORT, () =>{
+            console.log(`Server has started on PORT ${PORT}`);
+        })
+    } catch(e){
+        console.log(e);
+    }
+}
+
+start();
